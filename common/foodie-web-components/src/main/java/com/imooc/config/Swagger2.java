bd93b75e1778342c9399abfdc2c7ad0821de3a14
SIYUAN XU <xsiyuan@gmail.com>
2022-02-10 15:27:24 +0800

    创建web公共组件

---
+++ common/foodie-web-components/src/main/java/com/imooc/config/Swagger2.java
@@ -0,0 +1,39 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/** @author afu */
+@Configuration
+public class Swagger2 {
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("天天吃货 电商平台接口api") // 文档页标题
+        .contact(new Contact("imooc", "https://www.imooc.com", "abc@imooc.com")) // 联系人信息
+        .description("专为天天吃货提供的api文档") // 详细信息
+        .version("1.0.1") // 文档版本号
+        .termsOfServiceUrl("https://www.imooc.com") // 网站地址
+        .build();
+  }
+
+  @Bean
+  public Docket createRestestApi() {
+    return new Docket(DocumentationType.OAS_30)
+        // 用于定义api文档汇总信息
+        .apiInfo(apiInfo())
+        .select()
+        // 指定注解为restcontroller就会被抓取
+        .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+        // 所有controller
+        .paths(PathSelectors.any())
+        .build();
+  }
+}
---
+++ common/foodie-web-components/src/main/java/com/imooc/controller/BaseController.java
@@ -0,0 +1,64 @@
+package com.imooc.controller;
+
+import java.io.File;
+import org.springframework.stereotype.Controller;
+
+/** @author afu */
+@Controller
+public class BaseController {
+
+  public static final String FOODIE_SHOPCART = "shopcart";
+  public static final Integer COMMON_PAGE_SIZE = 10;
+  public static final Integer PAGE_SIZE = 20;
+  public static final String REDIS_USER_TOKEN = "redis_user_token";
+
+  /** 微信支付成功 -> 支付中心 -> shopping平台 // |-> 回调通知 URL */
+  String payReturnUrl = "http://localhost:8088//orders/notifyMerchantOrderPaid";
+  /** 支付中心的调用地址 */
+  String paymentUrl = "http://payment.t.mukewang.com/foodie-payment/payment/createMerchantOrder";
+  /** 用户上传头像的位置 */
+  public static final String IMAGE_USER_FACE_LOCATION =
+      File.separator
+          + "Users"
+          + File.separator
+          + "afu"
+          + File.separator
+          + "Workspace"
+          + File.separator
+          + "images"
+          + File.separator
+          + "foodie"
+          + File.separator
+          + "faces";
+
+  // FIXME: 需要移动到订单中心
+  /**
+   * 用于验证用户和订单是否有关联关系，避免非法用户调用
+   *
+   * @return 检查结果
+   */
+  //  public IMOOCJSONResult checkUserOrder(String userId, String orderId) {
+  //    Orders order = myOrdersService.queryMyOrder(userId, orderId);
+  //    if (order == null) {
+  //      return IMOOCJSONResult.errorMsg("订单不存在！");
+  //    }
+  //    return IMOOCJSONResult.ok(order);
+  //  }
+
+  /**
+   * 将用户数据转换为 redis 可用的 vo 对象
+   *
+   * @param user 用户数据
+   * @return redis 需要的 vo
+   */
+  //  public UsersVo conventUsersVo(Users user) {
+  //    // 实现用户的redis会话
+  //    String uniqueToken = UUID.randomUUID().toString().trim();
+  //    redisOperator.set(REDIS_USER_TOKEN + ":" + user.getId(), uniqueToken);
+  //
+  //    UsersVo usersVO = new UsersVo();
+  //    BeanUtils.copyProperties(user, usersVO);
+  //    usersVO.setUserUniqueToken(uniqueToken);
+  //    return usersVO;
+  //  }
+}
