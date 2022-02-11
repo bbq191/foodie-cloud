9b776c54ceae72742b3712c018cc42850e57bfbe
SIYUAN XU <xsiyuan@gmail.com>
2022-02-11 11:34:26 +0800

    创建商品中心

---
+++ domain/item/item-mapper/src/main/java/com/imooc/item/mapper/ItemsMapperCustom.java
@@ -0,0 +1,53 @@
+package com.imooc.item.mapper;
+
+import com.imooc.item.pojo.vo.ItemCommentVo;
+import com.imooc.item.pojo.vo.ShopCartVo;
+import java.util.List;
+import java.util.Map;
+import org.apache.ibatis.annotations.Param;
+
+/** @author afu */
+public interface ItemsMapperCustom {
+
+  /**
+   * 组装商品评价 vo
+   *
+   * @param map 参数列表
+   * @return 评价列表
+   */
+  List<ItemCommentVo> queryItemComments(@Param("paramsMap") Map<String, Object> map);
+  // todo: 迁移到主搜模块foodie-search
+  /**
+   * 组装查询商品 vo
+   *
+   * @param map 参数列表
+   * @return 商品列表
+   */
+  //  List<SearchItemsVo> searchItems(@Param("paramsMap") Map<String, Object> map);
+
+  /**
+   * 组装查询三级商品 vo
+   *
+   * @param map 参数列表
+   * @return 商品列表
+   */
+  //  List<SearchItemsVo> searchItemsByThirdCat(@Param("paramsMap") Map<String, Object> map);
+
+  /**
+   * 组装根据多个规格 id 查询 vo
+   *
+   * @param specIdsList 参数列表
+   * @return 商品列表
+   */
+  List<ShopCartVo> queryItemsBySpecIds(@Param("paramsList") List<String> specIdsList);
+
+  /**
+   * 减库存
+   *
+   * @param specId 规格 id
+   * @param pendingCounts 出库数量
+   * @return 剩余数量
+   */
+  int decreaseItemSpecStock(
+      @Param("specId") String specId, @Param("pendingCounts") int pendingCounts);
+}
---
+++ domain/item/item-mapper/src/main/java/com/imooc/item/mapper/ItemsSpecMapper.java
@@ -0,0 +1,7 @@
+package com.imooc.item.mapper;
+
+import com.imooc.item.pojo.ItemsSpec;
+import com.imooc.my.mapper.MyMapper;
+
+/** @author afu */
+public interface ItemsSpecMapper extends MyMapper<ItemsSpec> {}
