46ea202e7fe0303492c0a8ef924dce6a3a9f92b5
SIYUAN XU <xsiyuan@gmail.com>
2022-03-23 10:33:40 +0800

    增加 redis 限流

--- platform/gateway/src/main/java/com/imooc/RoutesConfiguration.java
+++ platform/gateway/src/main/java/com/imooc/RoutesConfiguration.java
@@ -1,12 +1,23 @@
 package com.imooc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 public class RoutesConfiguration {
+  @Autowired private KeyResolver hostNameResolver;
+
+  @Autowired
+  @Qualifier("redisLimiterUser")
+  private RateLimiter rateLimiterUser;
+
   @Bean
   public RouteLocator routes(RouteLocatorBuilder builder) {
     return builder
@@ -15,6 +26,14 @@ public class RoutesConfiguration {
         .route(
             r ->
                 r.path("/search/**", "/index/**", "/items/search", "/items/catItems")
+                    .filters(
+                        f ->
+                            f.requestRateLimiter(
+                                c -> {
+                                  c.setKeyResolver(hostNameResolver);
+                                  c.setRateLimiter(rateLimiterUser);
+                                  c.setStatusCode(HttpStatus.BAD_GATEWAY);
+                                }))
                     .uri("lb://FOODIE-SEARCH-SERVICE"))
         // 配置url pattern经常会漏掉某些字符导致转发出错，同学们发现视频里提到的那个导致错误的彩蛋了吗？
         .route(
